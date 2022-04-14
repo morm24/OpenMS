52d7559d99d8e9eb5c08df43d171b2e2aa24b891
Moritz Berger <morib70@nero.imp.fu-berlin.de>
2022-04-14 10:48:27 +0200

    Added class and header Colorizer.* into concept. Basic functionality for colorizing command line output (on Linux) is esablished

---
+++ src/openms/include/OpenMS/CONCEPT/Colorizer.h
@@ -0,0 +1,145 @@
+// --------------------------------------------------------------------------
+//                   OpenMS -- Open-Source Mass Spectrometry
+// --------------------------------------------------------------------------
+// Copyright The OpenMS Team -- Eberhard Karls University Tuebingen,
+// ETH Zurich, and Freie Universitaet Berlin 2002-2021.
+//
+// This software is released under a three-clause BSD license:
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+//  * Neither the name of any author or any participating institution
+//    may be used to endorse or promote products derived from this software
+//    without specific prior written permission.
+// For a full list of authors, refer to the file AUTHORS.
+// --------------------------------------------------------------------------
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL ANY OF THE AUTHORS OR THE CONTRIBUTING
+// INSTITUTIONS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+// OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+// OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+// ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// --------------------------------------------------------------------------
+// $Maintainer: Moritz Berger ; Tetana Krymovska$
+// $Authors: Moritz Berger ; Tetana Krymovska$
+// --------------------------------------------------------------------------
+
+#pragma once
+
+//#include <OpenMS/KERNEL/MSExperiment.h>
+
+#include <iosfwd>
+#include <sstream>
+#include <array>
+//#include <OPENMS_DLLAPI>
+
+
+
+
+
+namespace OpenMS
+{
+
+  /**
+   * @brief A class, that provides options for coloring String output with the "<<" Operator
+   *
+   */
+  class Colorizer
+  {
+
+  public:
+    /** @name Constructors and Destructor
+     */
+    //@{
+    Colorizer(int color); 
+
+    /// Copy constructor
+    //Colorizer(const Colorizer &rhs);
+
+    /// Default destructor
+    ~Colorizer();
+    //@}
+
+    
+    auto getColor(int i = NULL);
+    std::string getText();
+    bool getReset() {return this->reset;}
+
+//operator overloading
+    friend std::ostream &operator<<(std::ostream &o_stream, Colorizer& col);
+    template <typename T>
+    Colorizer& operator()(T s)
+    {
+      //clear was not possible (resets some flags in the sream)
+      this->reset = true;
+      this->_input.str("");
+      this->_input << s;
+      return *this;
+    }
+    Colorizer& operator()()
+    {
+      
+      this ->reset = false;
+      this->_input.str("");
+      return *this;
+    }
+
+  private:
+    //std::string _text;
+    const int _color;
+    std::stringstream _input;
+    bool reset = true;
+
+   //ewnum für farbauswahl
+
+#if defined(__linux__) || defined(__OSX__)
+    /**
+     * @brief constant string array which saves the Linux color codes.
+     * 0=black
+     * 1=red
+     * 2=green
+     * 3=yellow
+     * 4=blue
+     * 5=magenta
+     * 6=cyan
+     * 7=white
+     * 8=default console color (reset)
+     *
+     */
+    inline static const std::array<const char*,9> colors {"\033[30m", "\033[31m", "\033[32m", "\033[33m", "\033[34m", "\033[35m", "\033[36m", "\033[37m", "\033[0m"};
+    #elif defined(_WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(_WIN64)
+    inline static const std::array<const int,9> colors {16,12,10,14,9,13,11,15,15};
+#endif
+  };
+
+
+
+  extern /*OPENMS_DLLAPI*/ Colorizer black;
+  extern /*OPENMS_DLLAPI*/ Colorizer red;
+  extern /*OPENMS_DLLAPI*/ Colorizer green;
+  extern /*OPENMS_DLLAPI*/ Colorizer yellow;
+  extern /*OPENMS_DLLAPI*/ Colorizer blue;
+  extern /*OPENMS_DLLAPI*/ Colorizer magenta;
+  extern /*OPENMS_DLLAPI*/ Colorizer cyan;
+  extern /*OPENMS_DLLAPI*/ Colorizer white;
+  extern /*OPENMS_DLLAPI*/ Colorizer def;
+/*
+  Colorizer red(std::string text);
+  Colorizer green(std::string text);
+  Colorizer yellow(std::string text);
+  Colorizer cyan(std::string text);
+
+  //Colorizer blue(const char *text);
+  Colorizer red(const char *text);
+  Colorizer green(const char *text);
+  Colorizer yellow(const char *text);
+  Colorizer cyan(const char *text);*/
+}
---
+++ src/openms/source/CONCEPT/Colorizer.cpp
@@ -0,0 +1,154 @@
+// --------------------------------------------------------------------------
+//                   OpenMS -- Open-Source Mass Spectrometry
+// --------------------------------------------------------------------------
+// Copyright The OpenMS Team -- Eberhard Karls University Tuebingen,
+// ETH Zurich, and Freie Universitaet Berlin 2002-2021.
+//
+// This software is released under a three-clause BSD license:
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+//  * Neither the name of any author or any participating institution
+//    may be used to endorse or promote products derived from this software
+//    without specific prior written permission.
+// For a full list of authors, refer to the file AUTHORS.
+// --------------------------------------------------------------------------
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL ANY OF THE AUTHORS OR THE CONTRIBUTING
+// INSTITUTIONS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+// OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+// OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+// ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// --------------------------------------------------------------------------
+// $Maintainer: Moritz Berger; Tetana Krymovska $
+// $Authors: Moritz Berger ; Tetana Krymovska$
+// --------------------------------------------------------------------------
+
+#include </buffer/ag_bsc/pmsb_22/morib70/openms/work_in_progress/Colorizer.h>
+#include <iostream>
+
+#if defined(_WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(_WIN64)
+#include <windows.h>
+#endif
+/**
+
+#include <OpenMS/DATASTRUCTURES/String.h>
+
+#include <OpenMS/CONCEPT/Types.h>
+#include <OpenMS/CONCEPT/Exception.h>
+**/
+namespace OpenMS
+{
+
+  /** @name Constructors and Destructor
+   */
+  //@{
+  // constructor
+  Colorizer::Colorizer(int color): _color(color)
+  {
+  }
+
+  /// Default destructor
+  Colorizer::~Colorizer()
+  {
+  }
+  //@}
+
+  /**
+  \fn getColor(int i)
+  \details this function return the ANSI escape sequence for the color, saved in the Colorizer Object.
+  \return std::string contaaining ANSI escape characters for colors
+  \arg i codes for following colors:
+   * 0=black
+   * 1=red
+   * 2=green
+   * 3=yellow
+   * 4=blue
+   * 5=magenta
+   * 6=cyan
+   * 7=white
+   * 8=default console color (reset)
+  \return std::string contaaining ANSI escape characters for colors
+  */
+  auto Colorizer::getColor(int i)
+  {
+    //checking if parameter is outside array size or not set at all
+    // Darf nicht NULL, da dadurch kein "black" mehr möglich ist
+    return ((i == NULL || i > 8 || i < 0) ? this->colors[this->_color] : this->colors[i]);
+  }
+
+  std::string Colorizer::getText()
+  { 
+
+    return _input.str();
+  }
+  // overload the shift operator (<<)
+  std::ostream &operator<<(std::ostream &o_stream, OpenMS::Colorizer& col)
+  {
+    // colorize string with color set in the object
+#if defined(_WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(_WIN64)
+
+    // safe console Font info in Attributes
+    // could be set global, at programm start, to have less requests and faster access
+    CONSOLE_SCREEN_BUFFER_INFO Info;
+    HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+    GetConsoleScreenBufferInfo(hStdout, &Info);
+    WORD Attributes = Info.wAttributes;
+
+    
+    SetConsoleTextAttribute(hStdout,col.getColor());
+    
+    
+    // paste text
+    o_stream << col.getText();
+    // recover old Console font and set it as new one.
+    if(col.getReset()) 
+    {
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), Attributes);
+    }
+
+#elif defined(__linux__) || defined(__OSX__)
+    o_stream << col.getColor();
+
+    o_stream << col.getText();
+    //if flag reset is set: reset comand line. else dont reset.
+    o_stream << ((col.getReset())? col.getColor(8):"");
+#endif
+
+    return o_stream;
+  }
+  // All color types (Linux/OSX)
+  /*
+  Black           30  40
+  Red             31  41
+  Green           32  42
+  Yellow          33  43
+  Blue            34  44
+  Magenta         35  45
+  Cyan            36  46
+  White           37  47
+  */
+
+ //Objekte des typs colorizer
+  OpenMS::Colorizer black(0);
+  OpenMS::Colorizer red(1);
+  OpenMS::Colorizer green(2);
+  OpenMS::Colorizer yellow(3);
+  OpenMS::Colorizer blue(4);
+  OpenMS::Colorizer magenta(5);
+  OpenMS::Colorizer cyan(6);
+  OpenMS::Colorizer white(7);
+  OpenMS::Colorizer def(8);
+
+ //free funct. make_color
+
+
+}
